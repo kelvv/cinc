7798a0acd491b7cc44e7d1e4a65b2eed785b1b12
kelvv <kelvv@outlook.com>
2016-12-30 15:30:56 +0800

    add window.load

--- containers/totalPanel/totalPanel.js
+++ containers/totalPanel/totalPanel.js
@@ -29,35 +29,36 @@ function fetch (url, option) {
     }
   })
 }
+window.onload = function () {
+  let panels = document.getElementsByClassName('cinc-totalPanel')
+  let url = 'http://cinc.leanapp.cn/1.1/functions/totalCount'
+  for (let i = 0; i < panels.length; i++) {
+    let panel = panels[i]
+    let atts = ['user-id']
+    atts.forEach(att => {
+      if (!Object.prototype.hasOwnProperty.call(panel.attributes, att)) {
+        throw Error(`缺少 ${att} 属性`)
+      }
+    })
+    let body = `userId=${panel.attributes['user-id'].nodeValue}&&domain=${document.domain}`
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'X-AVOSCloud-Application-Id': 'GfIwMv2BxjSrE7IWjUDDjuLY-gzGzoHsz',
+        'X-AVOSCloud-Application-Key': '2Yxh385fxLsI1LqyxwFnJjWa'
+      },
+      body
+    }).then(function (res) {
+      let postCount = document.createElement('p')
+      postCount.textContent = `博文: ${res.result.postCount}篇`
+      panel.appendChild(postCount)
+      let readCount = document.createElement('p')
+      readCount.textContent = `阅读: ${res.result.readCount}次`
+      panel.appendChild(readCount)
+    }, function (e) {
 
-let panels = document.getElementsByClassName('cinc-totalPanel')
-let url = 'http://cinc.leanapp.cn/1.1/functions/totalCount'
-for (let i = 0; i < panels.length; i++) {
-  let panel = panels[i]
-  let atts = ['user-id']
-  atts.forEach(att => {
-    if (!Object.prototype.hasOwnProperty.call(panel.attributes, att)) {
-      throw Error(`缺少 ${att} 属性`)
-    }
-  })
-  let body = `userId=${panel.attributes['user-id'].nodeValue}&&domain=${document.domain}`
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'X-AVOSCloud-Application-Id': 'GfIwMv2BxjSrE7IWjUDDjuLY-gzGzoHsz',
-      'X-AVOSCloud-Application-Key': '2Yxh385fxLsI1LqyxwFnJjWa'
-    },
-    body
-  }).then(function (res) {
-    let postCount = document.createElement('p')
-    postCount.textContent = `博文: ${res.result.postCount}篇`
-    panel.appendChild(postCount)
-    let readCount = document.createElement('p')
-    readCount.textContent = `阅读: ${res.result.readCount}次`
-    panel.appendChild(readCount)
-  }, function (e) {
-
-  })
+    })
+  }
 }
 
